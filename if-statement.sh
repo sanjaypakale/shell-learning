c74856aa6999cbed0234cf56d722e6aab1a26a57
sanjay pakale <sanjaypakale@gmail.com>
2024-03-17 08:03:12 +0800

    if statement expression

--- if-statement.sh
+++ if-statement.sh
@@ -1,5 +1,27 @@
 #! /usr/bin
 
+#=====================================
+#   integer comparision
+#  -eq  - is equal to - if [ "$a" -eq "$b"]
+#  -ne  - is not equal to - if [ "$a" -ne "$b" ]
+#  -gt - is greater than - if [ "$a" -gt "$b" ]
+#  -lt - is less than -    if [ "$a" -lt "$b" ]
+#  -le - is less than or equal to - if [ "$a" -le "$b" ]
+#  < - is less than - (("$a" < "$b"))
+#  <= - is less than or equal to - (("$a" <= "$b"))
+#  > - is greater than - (("$a" > "$b"))
+#  >= - is greater than or equal to - (("$a" >= "$b"))
+
+#   string comparision
+# = - is equal to  - if [ "$a" = "$b" ]
+# == - is equal to - if [ "$a" == "$b" ]
+# != - is not equal to if [ "$a" != "$b" ]
+# < - is less than, in ASCII alphabetical order - if [ "$a" < "$b" ]
+# > - is greater than, in ASCII alphabetical order - if [ "$a" > "$b" ]
+# -z - string is null, that is has zero length
+
+#========================================================================
+
 count=10
 
 if [ $count -eq 9 ]
